d5da7a61102a3550f38a0acf0f3acb4440d36c3b
mofed <mofed.salah.hana@gmail.com>
2023-03-14 13:55:23 +0200

    sign or encoding token athorization and verfiy or decoding the token

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JWT-Basics
 Simple Project to show the JWT-Basics
+
+# JWT
+Authorization: when I created a username the token is generated and when i want to read a data like
+dashboard I will send a bareer token with request header to get our data
--- controllers/main.js
+++ controllers/main.js
@@ -1,16 +1,55 @@
 
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dashboard
 
-const login = async (req, res)=> {
-    res.send(req.body)
-}
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
 
+const login = async (req, res) => {
+    const { username, password } = req.body
+    // mongoose validation
+    // Joi
+    // check in the controller
+  
+    if (!username || !password) {
+        // I use Throw if i use instance of Error
+      throw new CustomAPIError('Please provide email and password')
+    }
+  
+    //just for demo, normally provided by DB!!!!
+    const id = new Date().getDate()
+  
+    // try to keep payload small, better experience for user
+    // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+    var token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn:'30d'});
+
+    res.status(200).json({ msg: 'user created', token })
+  }
+  
 
 const dashboard = async (req, res)=> {
-    const luckNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({
-        msg: 'Hello Mofed salah',
-        secret: `Here Is Your Authorized Data Your Lucky Number is ${luckNumber}`
-    })
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bareer')){
+        throw new CustomAPIError('No token provided', 401)
+    }
+
+    // console.log(authHeader);
+    // To Cut the Bareer from the token
+    const token = authHeader.split(' ')[1]
+    // console.log(token)
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const luckNumber = Math.floor(Math.random() * 100)
+        // console.log(decoded)
+        res.status(200).json({
+            msg: `Hello ${decoded.username}`,
+            secret: `Here Is Your Authorized Data Your Lucky Number is ${luckNumber}`
+        })
+    } catch (error) {
+        throw new CustomAPIError('No Authorized Route to access', 401)
+    }
 }
 
 module.exports = { 
